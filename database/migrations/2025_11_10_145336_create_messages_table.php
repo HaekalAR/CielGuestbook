93b3f4c907e32dc64325ca4593906895e42d8d46
HaekalAR <ekal26b@gmail.com>
2025-11-11 01:07:01 +0700

    fix

--- database/migrations/2025_11_10_145336_create_messages_table.php
+++ database/migrations/2025_11_10_145336_create_messages_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('messages', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('messages')) {
+            Schema::create('messages', function (Blueprint $table) {
+                $table->id();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
